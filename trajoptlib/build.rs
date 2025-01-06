25849f0c8fe32dfdebca62673eac42bab878d489
shueja-personal <j.a.shue@outlook.com>
2025-01-05 23:14:25 -0800

    Watch the whole trajoptlib/src (including c++) to recompile the trajoptlib crate on changes

--- trajoptlib/build.rs
+++ trajoptlib/build.rs
@@ -49,7 +49,5 @@ fn main() {
     println!("cargo:rustc-link-lib=Sleipnir");
     println!("cargo:rustc-link-lib=fmt");
 
-    println!("cargo:rerun-if-changed=src/RustFFI.hpp");
-    println!("cargo:rerun-if-changed=src/RustFFI.cpp");
-    println!("cargo:rerun-if-changed=src/lib.rs");
+    println!("cargo:rerun-if-changed=src");
 }
