02b95fb1eb061645b18e8584e08a03de09cb8fe7
shueja-personal <j.a.shue@outlook.com>
2025-07-28 18:15:30 -0700

    Delete old copies of apps

--- src-frontend/src/assets/GenerateInProgress.tsx
+++
@@ -1,9 +0,0 @@
-import { ShapeLine } from "@mui/icons-material";
-import IconInProgress from "./IconInProgress";
-
-function GenerateInProgress(props: any) {
-  return (
-    <IconInProgress {...props} icon={<ShapeLine></ShapeLine>}></IconInProgress>
-  );
-}
-export default GenerateInProgress;
