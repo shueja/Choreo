1e313342e8d26247073f9a161166307cab3e9f98
shueja-personal <j.a.shue@outlook.com>
2025-07-28 18:32:42 -0700

    Delete old copies of apps

--- src-frontend/src/assets/SaveInProgress.tsx
+++
@@ -1,7 +0,0 @@
-import { Save } from "@mui/icons-material";
-import IconInProgress from "./IconInProgress";
-
-function SaveInProgress(props: any) {
-  return <IconInProgress {...props} icon={<Save></Save>}></IconInProgress>;
-}
-export default SaveInProgress;
--- web/extension/constants.ts
+++
@@ -1,2 +0,0 @@
-export const EXT_NAME = "Svelte Extension";
-export const EXT_AUTHOR = "Franklin Shera";
